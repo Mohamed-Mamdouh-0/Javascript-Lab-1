a93ed85fe5e702361a137ec9dc70b8c1c95a6d45
Mohamed-Mamdouh-0 <mohamed.mamdouh.ss95@gmail.com>
2023-03-20 19:58:04 +0200

    finished Tasks

---
+++ myScript.js
@@ -0,0 +1,28 @@
+// first Task
+
+alert('welcome to my site');
+let firstTaskprompt = prompt('enter your name');
+alert('welcome ' + firstTaskprompt);
+
+// second task
+
+temp = 40;
+let weather = temp >= 30 ? 'Hot' : 'Cold';
+document.write(
+	"<h5> Today's temprature is " + temp + 'which is ' + weather + ' </h5>'
+);
+//  Third Task
+
+// let fname = prompt('type your name');
+// if (typeof fname === !String) {
+// 	fname = prompt('type your name');
+// }
+
+// let contactFn = ()=>{
+//     let birthDate = prompt("enter your birthdate")
+//     if(parseInt(birthDate) != NaN && parseInt(birthDate) < 2010){
+
+//     }else{
+//         birthDate = prompt("enter your birthdate")
+//     }
+// }
